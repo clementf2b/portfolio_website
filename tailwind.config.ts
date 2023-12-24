81ac32d87d0948453240561d67f97f709837d2d4
Clement Ng <ng2b30@hotmail.com>
2023-12-24 23:18:12 +0800

    fix for the error . Type 'number' is not assignable to type 'string'.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,8 +29,8 @@ const config: Config = {
       },
       keyframes: {
         fadeIn: {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
+          from: { opacity: "0" },
+          to: { opacity: "1" },
         },
         slideUp: {
           from: { transform: "translateY(100%)" },
